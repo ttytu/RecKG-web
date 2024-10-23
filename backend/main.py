984aca466a0d250ce65c18ee1ac3a83a96ad7f38
pushpull7381 <pushpull7381@gmail.com>
2024-10-23 08:00:31 +0000

    feat: Add function to upload files

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 import os
+import pandas as pd
 
-from fastapi import Depends, FastAPI, HTTPException, Security
+from fastapi import Depends, FastAPI, HTTPException, Security, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from typing import List
 from pydantic import BaseModel
-from pymongo import MongoClient
 from starlette.staticfiles import StaticFiles
+from db import *
 
 app = FastAPI(root_path="/api")
 
@@ -23,5 +25,23 @@ app.add_middleware(
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello World"}
+
+
+
+@app.post("/uploadfiles/")
+async def create_upload_file(    
+    user_file: UploadFile = File(...),
+    item_file: UploadFile = File(...),
+    interaction_file: UploadFile = File(...)
+):
+    db = DATABASE()
+
+    user_file_content = await user_file.read()
+    item_file_content = await item_file.read()
+    interaction_file_content = await interaction_file.read()
+    columns_list = db.put_data(user_file=user_file_content, 
+                               item_file=item_file_content, 
+                               interaction_file=interaction_file_content)
+    return columns_list
