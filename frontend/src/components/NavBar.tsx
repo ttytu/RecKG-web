d2196f1fdba8af7e2edfc24b3217236fb98851d8
ttytu <quixote1103@gmail.com>
2024-10-28 08:01:50 +0000

    navbar update

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,18 +4,14 @@ import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
 	return (
-		<nav className="sticky top-0 flex gap-4 p-4">
-			<div className="font-black">
+		<nav className="sticky top-0 flex gap-4 p-4 items-center">
+			<div className="font-black text-link">
 				<Link to="/">RecKG</Link>
 			</div>
-			<ul className="navbar-links flex gap-2">
-				<li>
-					<Link to="/graph">Graph</Link>
-				</li>
-				<li>
-					<Link to="/graphSVG">Graph SVG</Link>
-				</li>
-			</ul>
+			<div className="navbar-links flex gap-2 *:text-link text-sm">
+				<Link to="/upload">Upload</Link>
+				<Link to="/graph">Graph</Link>
+			</div>
 		</nav>
 	);
 };
