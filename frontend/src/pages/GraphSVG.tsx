1c54fb7aa94f1276044b92f2888c43410a8bb985
ttytu <quixote1103@gmail.com>
2024-10-28 08:01:36 +0000

    random graph irrelevant

--- frontend/src/pages/GraphSVG.tsx
+++ frontend/src/pages/GraphSVG.tsx
@@ -2,22 +2,6 @@ import ForceGraphSVG from "../components/ForceGraphSVG";
 import { testNodes, testEdges } from "./data";
 
 
-const generateRandomGraph = (numNodes: number, numLinks: number) => {
-	const nodes = Array.from({ length: numNodes }, (_, i) => ({
-		id: `Node ${i + 1}`,
-		group: Math.floor(Math.random() * 2),
-		title: `Node Title ${i + 1}`,
-	}));
-
-	const links = Array.from({ length: numLinks }, () => ({
-		source: `Node ${Math.floor(Math.random() * numNodes / 10) + 1}`,
-		target: `Node ${Math.floor(Math.random() * numNodes) + 1}`,
-		value: Math.floor(Math.random() * 5) + 1,
-	}));
-
-	return { nodes, links };
-};
-
 const mapData = (nodes: any[], links: any[]) => {
 	return {
 		nodes: nodes.map((node) => ({
@@ -34,13 +18,11 @@ const mapData = (nodes: any[], links: any[]) => {
 	};
 }
 
-const data = generateRandomGraph(200, 200);
-
 const GraphSVG = () => {
 	return (
 		<div className="w-full h-full flex gap-4 flex-col lg:flex-row">
 			<div className="lg:w-1/5">
-				<h1 className="text-2xl font-bold">Graph on SVG</h1>
+				<h1 className="text-2xl font-bold">Knowledge Graph</h1>
 				<p className="text-gray-500">Using D3</p>
 			</div>
 			<ForceGraphSVG data={mapData(testNodes, testEdges)} />
