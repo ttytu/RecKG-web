1a0205afb3f21d61c9f44b77aa5b660c9a7e09dd
ttytu <quixote1103@gmail.com>
2024-10-28 08:01:08 +0000

    uplaod page init

---
+++ frontend/src/pages/Upload.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Upload: React.FC = () => {
+	return (
+		<div className="w-full h-full flex gap-4 flex-col lg:flex-row">
+			<div className="lg:w-1/5">
+				<h1 className="text-2xl font-bold">Upload Your Dataset</h1>
+				<p className="text-gray-500">something</p>
+			</div>
+		</div>
+	);
+};
+
+export default Upload;
