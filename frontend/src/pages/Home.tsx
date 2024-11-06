b9c3c5d983ce5834d3b3f2632fc75ce1101e3e44
ttytu <quixote1103@gmail.com>
2024-11-06 04:49:44 +0000

    home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,7 @@
 function Home() {
 	return (
 		<div className="h-[90vh]">
-			<h1>Welcome to RecKG</h1>
-			<p>This is the homepage of the RecKG web application.</p>
+			<h1 className="text-3xl font-bold">Welcome to RecKG</h1>
 		</div>
 	);
 };
